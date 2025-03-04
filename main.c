5e968207828da6446e2654fdd60b6c1aa51e1355
신현석 <brassgum@gmail.com>
2025-03-04 17:28:40 +0900

    update polygon list

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
+#include <math.h>
+#include "def.h"
 #include "console_screen.h"
+#include "polygon.h"
 
 int main(void)
 {
-	init_screen(20, 10);
+	init_screen(40, 20);
 	
-	push_pixel('&', 19,9);
-	draw_screen();
+	push_pixel('2', 20, 10);
 
+	draw_screen();
 	release_screen();
 	return 0;
 }
