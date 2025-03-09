e38f3810cc1fc8d9755713013efbf934f32098b0
sunzx0428@gmail.com <57030648+sunzx0428@users.noreply.github.com>
2025-03-09 19:22:40 +0900

    player_Circle
    draw.

---
+++ player.h
@@ -0,0 +1,13 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+
+typedef struct {
+    int x;
+    int y;
+    int radius;
+} Player;
+
+void draw_player(int centerX, int centerY, int radius);
+
+#endif // PLAYER_H
